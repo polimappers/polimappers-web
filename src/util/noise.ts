b344d60863fbb463bf1f65ddbcc31d189a4a2aeb
darcros <darcros7@gmail.com>
2026-05-20 17:21:05 +0200

    WIP: isolines background

---
+++ src/util/noise.ts
@@ -0,0 +1,53 @@
+import alea from "alea";
+import { createNoise2D } from "simplex-noise";
+
+export interface SampleGridOptions {
+  width: number;
+  height: number;
+  cellSize: number;
+  xOff?: number;
+  yOff?: number;
+}
+
+export interface SampleGrid {
+  grid: number[][];
+  cellSize: number;
+}
+
+export type SampledFunction = (x: number, y: number) => number;
+
+export function sampleGrid(
+  fn: SampledFunction,
+  { width, height, cellSize, xOff = 0, yOff = 0 }: SampleGridOptions,
+) {
+  const cols = Math.ceil(width / cellSize) + 1;
+  const rows = Math.ceil(height / cellSize) + 1;
+  const grid: number[][] = [];
+
+  for (let u = 0; u < rows; u++) {
+    const row: number[] = [];
+    for (let v = 0; v < cols; v++) {
+      const x = v * cellSize + xOff;
+      const y = u * cellSize + yOff;
+      const value = fn(x, y);
+      row.push(value);
+    }
+    grid.push(row);
+  }
+
+  return { grid, cellSize };
+}
+
+export interface NoiseSettings {
+  seed: number;
+  scale: number;
+}
+
+export function simplexNoiseGrid(
+  { seed, scale }: NoiseSettings,
+  options: SampleGridOptions,
+) {
+  const prng = alea(seed);
+  const noise = createNoise2D(prng);
+  return sampleGrid((x, y) => noise(x * scale, y * scale), options);
+}
